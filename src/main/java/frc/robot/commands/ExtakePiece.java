35e61492553083c5dc97b0aba66d8c61e06d19fd
Oliver Wilde <ow26193@judgememorial.com>
2023-01-21 10:02:35 -0700

    made extake piece command

---
+++ src/main/java/frc/robot/commands/ExtakePiece.java
@@ -0,0 +1,38 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Intake;
+
+public class ExtakePiece extends CommandBase {
+
+    private final Intake m_intake;
+
+    public ExtakePiece(Intake subsystem) {
+        m_intake = subsystem;
+        addRequirements(m_intake);
+    }
+    
+    @Override
+    public void initialize() {
+    }
+
+    @Override
+    public void execute() {
+        m_intake.setIntakeMotor(-0.75);
+    }
+
+    @Override
+    public void end(boolean interrupted){
+        m_intake.stopIntakeMotor();
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+
+    @Override
+    public boolean runsWhenDisabled() {
+        return false;
+    }
+}
