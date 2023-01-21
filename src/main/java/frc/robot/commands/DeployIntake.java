d4ad2aa84c8710f490da235739e924c27cfd22c3
David Lupash <dl26118@judgememorial.com>
2023-01-21 09:33:41 -0700

    made intake piece command

--- src/main/java/frc/robot/commands/DeployIntake.java
+++ src/main/java/frc/robot/commands/DeployIntake.java
@@ -1,38 +1,34 @@
 package frc.robot.commands;
-
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Intake;
 
-public class IntakePiece extends CommandBase {
 
+public class DeployIntake extends CommandBase {
     private final Intake m_intake;
-
-    public IntakePiece(Intake subsystem) {
-
-         m_intake = subsystem;
+    public DeployIntake(Intake subsystem) {
+        m_intake = subsystem;
         addRequirements(m_intake);
-    
-    }
-    
-    @Override
-    public void initialize() {
-    }
 
+    }
+    public void initialize() {
+
+    }
     @Override
     public void execute() {
-        m_intake.setIntakeMotor(0.75);
+        m_intake.setIntakeSolinoid(Value.kForward);
     }
-    
+
     @Override
     public void end(boolean interrupted) {
-        m_intake.stopIntakeMotor();
+        m_intake.setIntakeSolinoid(Value.kOff);
     }
-    
+
     @Override
     public boolean isFinished() {
         return false;
     }
-    
+
     @Override
     public boolean runsWhenDisabled() {
         return false;
