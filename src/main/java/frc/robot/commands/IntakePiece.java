570f6f7ba58ce391c465fd6f6a3a415ce46f6d6d
Jay Kyriakakis <jk24202@judgememorial.com>
2023-01-20 17:26:09 -0700

    I did it, made the intake piece command! ;D

---
+++ src/main/java/frc/robot/commands/IntakePiece.java
@@ -0,0 +1,36 @@
+package frc.robot.commands;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Intake;
+
+
+public class IntakePiece extends CommandBase {
+    private final Intake m_intake;
+    public IntakePiece(Intake subsystem) {
+        m_intake = subsystem;
+        addRequirements(m_intake);
+
+    }
+    public void initialize() {
+
+    }
+    @Override
+    public void execute() {
+        m_intake.setIntakeSolinoid(Value.kForward);
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        m_intake.setIntakeSolinoid(Value.kOff);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+
+    @Override
+    public boolean runsWhenDisabled() {
+        return false;
+    }
+}
