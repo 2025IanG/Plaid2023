ba7759b780afa7ac497603557c0f36db8932f3cd
2025IanG <ig25049@judgememorial.com>
2023-02-18 10:20:28 -0800

    Still finnagling with the values Rechecked some math and changed values accordingly

--- src/main/java/frc/robot/commands/AutoPaths.java
+++ src/main/java/frc/robot/commands/AutoPaths.java
@@ -46,12 +46,12 @@ public class AutoPaths extends CommandBase{
             new WaitCommand(w),
             new ExtakePiece(m_intake).withTimeout(1.5),
             new WaitCommand(w),
-            new DriveStraight(m_drive, 0.25, 500),
+            new DriveStraight(m_drive, 0.25, 842),
             // new DriveStraight(m_drive, 0.25).withTimeout(3),
-            new WaitCommand(4 * w),
-            new DriveStraight(m_drive, 0.25, -200),
-            new WaitCommand(4 * w),
-            new DriveStraight(m_drive, 0.25, 100)
+            new WaitCommand(w),
+            new DriveStraight(m_drive, 0.25, -275),
+            new WaitCommand(w),
+            new DriveStraight(m_drive, 0.25, 0)
         );
     }
 
